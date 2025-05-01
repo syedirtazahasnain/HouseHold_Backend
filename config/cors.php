37656a7e924ed8b19b233f0f98986dc55dbf077e
Irtaza <arsal.h@yahoo.com>
2025-05-02 00:30:06 +0500

    updating the cors path

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000', 'http://localhost:3001', 'http://127.0.0.1:3000'],
+    'allowed_origins' => ['http://localhost:3000', 'http://localhost:3001', 'http://127.0.0.1:3000', 'http://192.168.100.15:3001', 'http://192.168.100.15'],
 
     'allowed_origins_patterns' => [],
 
