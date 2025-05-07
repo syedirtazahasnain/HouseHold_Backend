d2db350a95c2aa2b709d6154ddd36c8f3fe221f1
Irtaza <arsal.h@yahoo.com>
2025-05-07 19:58:18 +0500

    added employe id and shiow current order on dashboard page , also , permanent employees take cash

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -19,6 +19,7 @@ class UserResource extends JsonResource
             'name' => $this->name,
             'email' => $this->email,
             'my_role' => $this->role,
+            'emp_id' => $this->emp_id,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
         ];
